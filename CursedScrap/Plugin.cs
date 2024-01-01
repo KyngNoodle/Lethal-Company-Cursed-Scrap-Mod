e99e102e27b6f1dab1af38adf69b35d3d6ca4bab
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-01 03:36:11 +0000

    add log-lines

--- CursedScrap/Plugin.cs
+++ CursedScrap/Plugin.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 
 namespace CursedScrap;
@@ -7,6 +8,13 @@ namespace CursedScrap;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class CursedScrapPlugin : BaseUnityPlugin
 {
+    internal new static ManualLogSource Logger = null!;
+
+    public CursedScrapPlugin()
+    {
+        Logger = base.Logger;
+    }
+    
     public void Awake()
     {
         var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
@@ -17,9 +25,13 @@ public class CursedScrapPlugin : BaseUnityPlugin
     [HarmonyPrefix]
     public static void RemoveNotSillyScraps(RoundManager __instance)
     {
+        Logger.LogInfo("Doing the cursed scrap...");
+        
         var moonThatCanSpawnAllSillyScraps = __instance.playersManager.levels.First(
             level => level.PlanetName == "Experimentation"
         );
+        
+        Logger.LogInfo($"Found experimentation moon {moonThatCanSpawnAllSillyScraps}");
         
         var sillyScraps = moonThatCanSpawnAllSillyScraps.spawnableScrap
             .Where(scrap => scrap.spawnableItem.name is "Jar of pickles" or "Bottles")
@@ -28,6 +40,8 @@ public class CursedScrapPlugin : BaseUnityPlugin
                 return scrap;
             })
             .ToList();
+        
+        Logger.LogInfo($"Found {sillyScraps.Count} scraps");
         
         __instance.playersManager.levels.Do(
             level => {
